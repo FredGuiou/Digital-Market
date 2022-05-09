825fbc4f9736a7e06ad613ef9e807562379add38
FredGuiou <frederic.guiou@gmail.com>
2022-05-09 09:30:39 +0200

    Etape 6 correction affichage Nom du produit - oneProduct.title et pas name

--- app/controllers/sessionController.js
+++ app/controllers/sessionController.js
@@ -1,5 +1,8 @@
 const bcrypt = require('bcrypt');
-const { User, Role } = require('../models');
+const {
+    User,
+    Role
+} = require('../models');
 
 const sessionController = {
     index: (req, res) => {
@@ -9,25 +12,54 @@ const sessionController = {
     // post data here and create session
     login: async (req, res) => {
         try {
-            const { email, password } = req.body;
+            const {
+                email,
+                password
+            } = req.body;
             // !! Votre code à partir d'ici
 
             // On récupère user avec le role
+            const user = await User.findOne({
+                where: {
+                    email,
+                    password
+                }
+            });
             // Est-ce que l'utilisateur existe en BDD ?
-            // Sélectionner user avec email et inclure le role, si on ne le trouve pas :
-            //      on envoie un message d'erreur dans un objet:  {error: `l'utilisateur n'existe pas !`} et on render `login` en lui passant l'erreur
-            // Sinon on continue.
+            if (user) { // SI USER TROUVE
+                // Sélectionner user avec email et inclure le role, si on ne le trouve pas :
+                user = await User.findOne({
+                    where: {
+                        email,
+                        password,
+                        role_id
+                    }
+                });
+            } else {
+                const { error } = res.send("l'utilisateur n'existe pas");
+                
+                res.render('login', { error });
+                //      on envoie un message d'erreur dans un objet:  {error: `l'utilisateur n'existe pas !`} et on render `login` en lui passant l'erreur
+                // Sinon on continue.
+            }
+            if (user) {
+                // Le mot de passe est il correct ?
+                const isMatch = await bcrypt.compare(req.body.password, user.password);
+                // On compare le mots de passe du formulaire avec celui de l'utilisateur
+                if (!isMatch) {
+                    //      Si le mot de passe est incorrect : on envoie un message d'erreur dans un objet:  {error: `Mot de passe incorrect`} et on render `login` en lui passant l'erreur
+                    const { error } = res.send("Mot de passe incorrect");
 
-            // Le mot de passe est il correct ?
-            // On compare le mots de passe du formulaire avec celui de l'utilisateur
-            //      Si le mot de passe est incorrect : on envoie un message d'erreur dans un objet:  {error: `Mot de passe incorrect`} et on render `login` en lui passant l'erreur
-
-            // On ajoute user a la session
-
-            // On enlève le mot de passe de la session.
-
-            // !! Ne pas modifier cette ligne
-            res.redirect('/');
+                    res.render('login', { error });
+                } else {
+                    // On ajoute user a la session
+                    user = req.session.user;
+                    // On enlève le mot de passe de la session.
+                    delete req.session.user.password;
+                    // !! Ne pas modifier cette ligne
+                    res.redirect('/');
+                }
+            }
         } catch (e) {
             console.error(e.message);
             res.status(500).send('Server Error');
